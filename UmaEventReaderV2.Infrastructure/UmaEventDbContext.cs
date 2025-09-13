78452a07e5360d2570e61987e75eb9bbd1170a2e
lucad <luca@trenz.de>
2025-09-13 20:33:28 +0200

    added infra project for migrations db context and other helpers

---
+++ UmaEventReaderV2.Infrastructure/UmaEventDbContext.cs
@@ -0,0 +1,28 @@
+using UmaEventReaderV2.Models.Entities;
+
+namespace UmaEventReaderV2.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+
+public class UmaContext : DbContext
+{
+    public DbSet<UmaEventEntity> Events => Set<UmaEventEntity>();
+    public DbSet<UmaEventChoiceEntity> Choices => Set<UmaEventChoiceEntity>();
+    public DbSet<UmaEventChoiceOutcomeEntity> Outcomes => Set<UmaEventChoiceOutcomeEntity>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Port=5432;Database=uma_db;Username=umauser;Password=umapassword");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<UmaEventChoiceOutcomeEntity>()
+            .Property(o => o.Type)
+            .HasConversion<string>(); // store enum as string instead of int
+
+        base.OnModelCreating(modelBuilder);
+    }
+}

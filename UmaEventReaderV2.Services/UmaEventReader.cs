318011f4f82ab09b0085de40ace5cd3359987076
lucad <luca@trenz.de>
2025-09-14 02:18:53 +0200

    tested with new params

--- UmaEventReaderV2.Services/UmaEventReader.cs
+++ UmaEventReaderV2.Services/UmaEventReader.cs
@@ -5,15 +5,19 @@ namespace UmaEventReaderV2.Services;
 /// <summary>
 /// Main running loop of the application
 /// </summary>
-public class UmaEventReader(IUmaEventService eventService)
+public class UmaEventReader(IUmaEventService eventService, IScreenshotAreaProvider screenshotAreaProvider)
 {
     public void Run()
     {
-        var text = "i would";
+        var area = screenshotAreaProvider.GetEventArea();
 
-        var events = eventService.GetAllWhereChoiceTextIsLike(text);
+        Console.Out.WriteLine("Area" + area);;
 
-        foreach (var e in events)
-            Console.WriteLine(e);
+        // var text = "i would";
+        //
+        // var events = eventService.GetAllWhereChoiceTextIsLike(text);
+        //
+        // foreach (var e in events)
+        //     Console.WriteLine(e);
     }
 }

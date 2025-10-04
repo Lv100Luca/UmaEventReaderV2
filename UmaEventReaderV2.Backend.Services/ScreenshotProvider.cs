e2c5d6efea9dc9dc8b0436920145974bd307961b
lucad <luca@trenz.de>
2025-10-04 23:38:37 +0200

    made screenshotting more resilient

--- UmaEventReaderV2.Backend.Services/ScreenshotProvider.cs
+++ UmaEventReaderV2.Backend.Services/ScreenshotProvider.cs
@@ -1,24 +1,29 @@
 using System.Drawing;
+using Microsoft.Extensions.Logging;
 using UmaEventReaderV2.Abstractions;
 
 namespace UmaEventReaderV2.Services;
 
-public class ScreenshotProvider : IScreenshotProvider
+public class ScreenshotProvider(ILogger<ScreenshotProvider> logger) : IScreenshotProvider
 {
     public Bitmap? TakeScreenshot(Rectangle area)
     {
         if (area.Width <= 0 || area.Height <= 0)
             return null;
 
-        var bmp = new Bitmap(area.Width, area.Height);
-        using var g = Graphics.FromImage(bmp);
-        g.CopyFromScreen(area.Location, Point.Empty, area.Size);
+        try
+        {
+            var bmp = new Bitmap(area.Width, area.Height);
+            using var g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(area.Location, Point.Empty, area.Size);
 
-        var a = false;
+            return bmp;
+        }
+        catch (Exception e)
+        {
+            logger.LogWarning(e, "Failed to take screenshot");
 
-        if (a)
-            throw new Exception("Not implemented");
-
-        return bmp;
+            return null;
+        }
     }
 }
